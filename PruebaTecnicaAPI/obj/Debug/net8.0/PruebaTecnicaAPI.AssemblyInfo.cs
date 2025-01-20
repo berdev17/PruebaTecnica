f71cff97ad1cbb5b51c1d317663bf205c39e510a
Bernardo <ortizbernardo17f@gmail.com>
2025-01-20 11:16:57 -0400

    Primer Commit

--- PruebaTecnicaAPI/obj/Debug/net8.0/PruebaTecnicaAPI.AssemblyInfo.cs
+++ PruebaTecnicaAPI/obj/Debug/net8.0/PruebaTecnicaAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PruebaTecnicaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebf9f7e02df5d99912dd2834a8c184ab2ff827f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PruebaTecnicaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PruebaTecnicaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
